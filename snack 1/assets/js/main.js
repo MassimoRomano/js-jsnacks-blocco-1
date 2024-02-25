51d3d57ce7369b2739f004c26947d811df1b595c
MassimoRomano <romano93m@gmail.com>
2024-02-25 15:36:48 +0100

    add covert string in number

--- snack 1/assets/js/main.js
+++ snack 1/assets/js/main.js
@@ -1,3 +1,13 @@
 //ask the user to write two numbers
 var firstNumber = prompt("Inserisci il primo numero")
 var secondNumber = prompt("Inserisci il secondo numero")
+
+console.log("firstNumber, secondNumber");
+
+
+// Convert string in number
+var convertFirst = Number(firstNumber);
+var convertSecond = Number(secondNumber);
+
+
+console.log("convertFirst,convertSecond");
