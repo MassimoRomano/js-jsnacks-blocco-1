3fca62d809731de812c00ff99216b3cd96f9c19b
MassimoRomano <romano93m@gmail.com>
2024-02-25 16:12:06 +0100

    add invited name

--- snack 4/assets/js/main.js
+++ snack 4/assets/js/main.js
@@ -1,2 +1,9 @@
 //Invited list
-let invitateName = ["Michele", "Giuseppe", "Antonio", "Massimo"];
+let invitedList = ["Michele", "Giuseppe", "Antonio", "Massimo"];
+
+console.log(invitedList);
+
+//Invited name
+let invitedName = prompt("Inserisci il tuo nome");
+
+console.log(invitedName);
