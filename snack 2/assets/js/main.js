70e9a641027dc3e422949c9e1943e9a8eb68efbb
MassimoRomano <romano93m@gmail.com>
2024-02-25 16:01:44 +0100

    add if else

--- snack 2/assets/js/main.js
+++ snack 2/assets/js/main.js
@@ -3,7 +3,22 @@ var firstWord = prompt("Inserisci la prima parola:");
 // ask to user second word
 var secondWord = prompt("Inserisci la seconda parola:");
 
+console.log("firstWord, secondWord");
+
 //identifies the longest word
 var lenghtFirst = firstWord.length
 var lenghtSecond = secondWord.length
 
+console.log("lenghtFirst,lenghtSecond");
+
+
+// Prints the shortest and longest word
+if (lenghtFirst < lenghtSecond) {
+    console.log("La parola più corta è: " + firstWord);
+    console.log("La parola più lunga è: " + secondWord);
+} else if (lenghtSecond < lenghtFirst) {
+    console.log("La parola più corta è: " + secondWord);
+    console.log("La parola più lunga è: " + firstWord);
+} else {
+    console.log("Le parole hanno la stessa lunghezza.");
+}
