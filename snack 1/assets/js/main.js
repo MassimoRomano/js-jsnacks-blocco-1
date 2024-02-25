81a590f30833433f4150c870d64cb09880051712
MassimoRomano <romano93m@gmail.com>
2024-02-25 16:07:11 +0100

    fix exercise with let

--- snack 1/assets/js/main.js
+++ snack 1/assets/js/main.js
@@ -1,13 +1,13 @@
 //ask the user to write two numbers
-var firstNumber = prompt("Inserisci il primo numero")
-var secondNumber = prompt("Inserisci il secondo numero")
+let firstNumber = prompt("Inserisci il primo numero")
+let secondNumber = prompt("Inserisci il secondo numero")
 
 console.log("firstNumber, secondNumber");
 
 
 // Convert string in number
-var convertFirst = Number(firstNumber);
-var convertSecond = Number(secondNumber);
+let convertFirst = Number(firstNumber);
+let convertSecond = Number(secondNumber);
 
 
 console.log("convertFirst,convertSecond");
--- snack 2/assets/js/main.js
+++ snack 2/assets/js/main.js
@@ -1,13 +1,13 @@
 // ask to user first word
-var firstWord = prompt("Inserisci la prima parola:");
+let firstWord = prompt("Inserisci la prima parola:");
 // ask to user second word
-var secondWord = prompt("Inserisci la seconda parola:");
+let secondWord = prompt("Inserisci la seconda parola:");
 
 console.log("firstWord, secondWord");
 
 //identifies the longest word
-var lenghtFirst = firstWord.length
-var lenghtSecond = secondWord.length
+let lenghtFirst = firstWord.length
+let lenghtSecond = secondWord.length
 
 console.log("lenghtFirst,lenghtSecond");
 
