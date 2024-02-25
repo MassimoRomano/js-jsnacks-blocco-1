31a38129c33e831822de59c039165c7f4a30212e
MassimoRomano <romano93m@gmail.com>
2024-02-25 16:23:50 +0100

    add cycle for

--- snack 4/assets/js/main.js
+++ snack 4/assets/js/main.js
@@ -7,3 +7,12 @@ console.log(invitedList);
 let invitedName = prompt("Inserisci il tuo nome");
 
 console.log(invitedName);
+
+//I scroll through the guest list to see if the name is there
+
+for(let i = 0 ; i < invitedList.leght ; i++){
+    if(invitedList[i] === invitedName){
+    invitedFound = true
+    break // close cicle for
+    }
+}
