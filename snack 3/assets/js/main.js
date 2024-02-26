14d9b66b07152c07c5f24b2532c56719b73c9917
MassimoRomano <romano93m@gmail.com>
2024-02-26 10:56:13 +0100

    add snack 3 cycle while

--- snack 3/assets/js/main.js
+++ snack 3/assets/js/main.js
@@ -1,8 +1,19 @@
-let somma = 0;
+/* let somma = 0;
 
 for (let i = 0; i < 10; i++) {
     let numero = +prompt("Scrivi un numero");
     somma += numero;
+}
+
+console.log("La somma dei tuoi numeri è: " + somma); */
+
+let somma = 0;
+let contatore = 0;
+
+while (contatore < 10) {
+    let numero = +prompt("Scrivi un numero");
+    somma += numero;
+    contatore++;
 }
 
 console.log("La somma dei tuoi numeri è: " + somma);
