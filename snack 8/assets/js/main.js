1765c9650b9cbfd4ed5b72abdedd8de76b6251f2
MassimoRomano <romano93m@gmail.com>
2024-02-26 22:44:26 +0100

    add snack 8 cycle while

--- snack 8/assets/js/main.js
+++ snack 8/assets/js/main.js
@@ -1,4 +1,4 @@
-let somma = 0; 
+/* let somma = 0; 
 
 let userNumber = prompt (' Inserisci un numero di 4 cifre');
 
@@ -10,6 +10,21 @@ for (let i = 0; i < userNumber.length; i++) {
     
     somma += element;
     
+}
+
+console.log("La somma delle cifre di", userNumber, "è:", somma); */
+
+
+//cycle while
+
+let somma = 0;
+let userNumber = prompt('Inserisci un numero di 4 cifre:');
+
+let i = 0;
+while (i < userNumber.length) {
+    let cifra = Number(userNumber[i]);
+    somma += cifra;
+    i++;
 }
 
 console.log("La somma delle cifre di", userNumber, "è:", somma);
